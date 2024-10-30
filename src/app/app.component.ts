5a2de9699993625534c9d309661a579c2553c124
LivAn <panlivan1234@gmail.com>
2024-10-30 15:01:17 +0700

    I edition my component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReceiveOderListComponent } from './receive-oder-list/receive-oder-list.component';
-import { OrderDetailComponent } from "./order-detail/order-detail.component";
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 import { REjectOrderComponent } from './reject-order/reject-order.component';
+import { ReceiveOrderListComponent } from './receive-order-list/receive-order-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReceiveOderListComponent, OrderDetailComponent,REjectOrderComponent],
+  imports: [
+    RouterOutlet,
+    ReceiveOderListComponent,
+    OrderDetailComponent,
+    REjectOrderComponent,
+    ReceiveOrderListComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Anglar1_Pro_Management';
