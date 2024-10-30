b55296ea19de0124f4815e290c51f7b32d8017ac
rebeca <rebecachur11@gmail.com>
2024-10-30 15:02:25 +0700

    i add return component HTML and CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { ReceiveOderListComponent } from './receive-oder-list/receive-oder-list.component';
 import { OrderDetailComponent } from "./order-detail/order-detail.component";
 import { REjectOrderComponent } from './reject-order/reject-order.component';
+import { ReturnComponent } from './return/return.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReceiveOderListComponent, OrderDetailComponent,REjectOrderComponent],
+  imports: [RouterOutlet, ReceiveOderListComponent, OrderDetailComponent,REjectOrderComponent
+    ,ReturnComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
